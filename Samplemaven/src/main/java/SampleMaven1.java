2f3ed3a1ad1e320df2ec98b2baa6b6aebd951a64
Vismitha <Vismitha@LAPTOP-36QH3DNI>
2024-01-04 12:14:11 +0530

    First commit

---
+++ Samplemaven/src/main/java/SampleMaven1.java
@@ -0,0 +1,14 @@
+
+public class SampleMaven1 
+{
+	public void add()
+	{
+	System.out.println("Hello world");	
+	}
+	public static void main(String []args)
+	{
+		SampleMaven1 sa=new SampleMaven1();
+		sa.add();
+	}
+	}
+
---
+++ Samplemaven/src/main/java/javamaven/MavenSample1.java
@@ -0,0 +1,15 @@
+package javamaven;
+
+public class MavenSample1 {
+	public void add()
+	{
+	System.out.println("Hello world");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MavenSample1 sa=new MavenSample1();
+		sa.add();
+	}
+
+}
